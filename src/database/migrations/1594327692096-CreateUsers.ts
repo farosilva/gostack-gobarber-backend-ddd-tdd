b3ce66ddd245d9c9529e3939e516bcad4504c557
Fabricio Silva <fabricio.roddrigo@gmail.com>
2020-07-10 16:51:38 -0300

    Created relationships with models

--- src/database/migrations/1594327692096-CreateUsers.ts
+++ src/database/migrations/1594327692096-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1594327692096 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
