b9978c61c462137599ffaaa362dcca0397ff1dfd
Fabricio Silva <fabricio.roddrigo@gmail.com>
2020-07-10 22:26:39 -0300

    Created a global middleware for errors

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,22 +5,18 @@ import AuthenticatedUserService from '../services/AuthenticatedUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticatedUser = new AuthenticatedUserService();
+  const authenticatedUser = new AuthenticatedUserService();
 
-    const { user, token } = await authenticatedUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticatedUser.execute({
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ message: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors'; // Pacote instalado para pegar os errors das rotas assincronas.
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -11,6 +15,23 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+// Middleware para tratativa de erros
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('✨ Server started on port 3333!');
