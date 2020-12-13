1464b13617b60532204d2590c42bfbf629fa0156
Fabricio Silva <fabricio.roddrigo@gmail.com>
2020-12-13 15:19:05 -0300

    Listing days avaiables

--- src/modules/appointments/services/ListProvidersMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProvidersMonthAvailabilityService.spec.ts
@@ -20,7 +20,47 @@ describe('ListProviders', () => {
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      date: new Date(2020, 4, 20, 9, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
       date: new Date(2020, 4, 20, 10, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 11, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 12, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 13, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 16, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 17, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
@@ -34,12 +74,12 @@ describe('ListProviders', () => {
       month: 5,
     });
 
-    expect(availability).toEqual(
+    await expect(availability).toEqual(
       expect.arrayContaining([
         { day: 19, available: true },
         { day: 20, available: false },
         { day: 21, available: true },
-        { day: 22, available: false },
+        { day: 22, available: true },
       ]),
     );
   });
